f673eaf0ef2975f8b33acfb2a2f3865c35c27da1
Veysi yê MALA SAF <veysieken000@gmail.com>
2026-07-02 09:40:41 +0300

    feat(isabelle): assemble Isabelle formalization entry point

---
+++ isabelle/Main.thy
@@ -0,0 +1,30 @@
+theory Main
+  imports
+    Core
+    Primitive
+    StateSpace
+    Operators
+    Constraints
+    Objectives
+    Invariants
+    Yek
+    UnifiedSystem
+    Theorems
+    Proofs
+begin
+
+thm UnifiedCorrectness
+
+thm Soundness
+
+thm Feasibility
+
+thm Determinism
+
+thm Verification
+
+thm YekPreservation
+
+end
+
+
